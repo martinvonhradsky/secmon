c4aa0c0a297ccabe0c2e9ae84b9e5a01557bafec
Martin Hradsky <martin.hradsky72@gmail.com>
2023-05-17 22:54:18 +0200

    mitre module ip configurable

--- config/params.php
+++ config/params.php
@@ -2,5 +2,6 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-    'defaultPass' => 'password'
+    'defaultPass' => 'password',
+    'mitreModuleAddress' => 'http://192.168.0.150:8085'
 ];
--- views/mitre/index.php
+++ views/mitre/index.php
@@ -1,15 +1,10 @@
 <?php
+use Yii;
 
-use macgyer\yii2materializecss\widgets\grid\GridView;
-use yii\widgets\Pjax;
-use kartik\cmenu\ContextMenu;
-
-/* @var $this yii\web\View */
-/* @var $searchModel app\models\NetworkModelSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
+$mitreModuleAddress = Yii::$app->params['mitreModuleAddress'];
 
 $this->params['title'] = 'Mitre module';
 ?>
-<iframe src='http://192.168.0.150:8085' width='100%' height='90%' style="broder:none"></iframe>
+<iframe src='<?= $mitreModuleAddress ?>'  width='100%' height='90%' style="border:none"></iframe>
 
 
